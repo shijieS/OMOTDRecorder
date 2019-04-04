eb40f3214f0dc61ac067a482fc4116393fce2d91
shijie <shijieSun@chd.edu.cn>
2019-04-04 16:07:51 +0800

    remove fix fps, for not working

--- start_carla.sh
+++ start_carla.sh
@@ -1,7 +1,7 @@
 echo "==================Start Create Simulator=================="
-x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/CARLA_0.9.4; ./CarlaUE4.sh Town02 -carla-server-timeout=500000ms -quality-level=Epic -carla-port=2000 -benchmark -fps=30"
-x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/CARLA_0.9.4; ./CarlaUE4.sh Town04 -carla-server-timeout=500000ms -quality-level=Epic -carla-port=2010 -benchmark -fps=30"
-x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/CARLA_0.9.4; ./CarlaUE4.sh Town05 -carla-server-timeout=500000ms -quality-level=Epic -carla-port=2020 -benchmark -fps=30"
+x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/CARLA_0.9.4; ./CarlaUE4.sh Town02 -carla-server-timeout=500000ms -quality-level=Epic -carla-port=2000 -benchmark"
+x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/CARLA_0.9.4; ./CarlaUE4.sh Town04 -carla-server-timeout=500000ms -quality-level=Epic -carla-port=2010 -benchmark"
+x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/CARLA_0.9.4; ./CarlaUE4.sh Town05 -carla-server-timeout=500000ms -quality-level=Epic -carla-port=2020 -benchmark"
 
 echo "==================Successfully Create Simulator=================="
 echo "==================Wait ....=================="
@@ -10,8 +10,8 @@ sleep 20
 echo "==================Start Recording Script=================="
 
 
-x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/AMOTDRecorder; source activate CARLA; /home/ssj/miniconda3/envs/CARLA/bin/python start_recording.py --port=2000 --config_name=Town02_all.json --recording_num_scale=0.5"
-x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/AMOTDRecorder; source activate CARLA; /home/ssj/miniconda3/envs/CARLA/bin/python start_recording.py --port=2010 --config_name=Town04_all.json --recording_num_scale=0.5"
-x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/AMOTDRecorder; source activate CARLA; /home/ssj/miniconda3/envs/CARLA/bin/python start_recording.py --port=2020 --config_name=Town05_all.json --recording_num_scale=0.5"
+x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/AMOTDRecorder; /home/ssj/miniconda3/envs/CARLA/bin/python start_recording.py --port=2000 --config_name=Town02_all.json --recording_num_scale=0.5"
+x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/AMOTDRecorder; /home/ssj/miniconda3/envs/CARLA/bin/python start_recording.py --port=2010 --config_name=Town04_all.json --recording_num_scale=0.5"
+x-terminal-emulator -e sh -c "cd /home/ssj/Data/github/AMOTDRecorder; /home/ssj/miniconda3/envs/CARLA/bin/python start_recording.py --port=2020 --config_name=Town05_all.json --recording_num_scale=0.5"
 
 echo "==================Well Done!!=================="
